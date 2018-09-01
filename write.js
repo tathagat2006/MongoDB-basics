110edd04fa607e6a1bfe9946c198fb0cd842b754
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-09-01 16:26:30 +0530

    writing a task to db using insertone and insertmany
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- write.js
+++ write.js
@@ -1,2 +1,24 @@
 // writing to our mongodb database.
 
+const {MongoClient} = require('mongodb')
+
+const MONGO_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'tododb';
+
+async function writeTask() {
+
+    const client = await MongoClient.connect(MONGO_URL)
+    const tododb = client.db(DB_NAME)
+
+    const todos = tododb.collection('todos')
+    const todo = {
+        task: 'an imp task',
+        priority: 2,
+        owner: 'manager'
+    }
+    const result = await todos.insertOne(todo)
+    console.log(result)
+
+}
+
+writeTask()
